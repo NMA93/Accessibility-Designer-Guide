f449a946d6c98b237550bb2ab3ce2432ee5ca9cc
NMA93 <mail@nilsmaeder.ch>
2020-05-29 16:39:38 +0200

    main.js

---
+++ _site/assets/js/main.js
@@ -0,0 +1,15 @@
+// $(document).ready(function(){ $("#container1").twentytwenty(); });
+
+// $(window).load(function(){ 
+//   $("#container1").twentytwenty(); 
+//   });
+
+window.addEventListener("load",function(){ 
+    $("#container1").twentytwenty(); 
+  });
+
+
+
+// $(function(){
+//   setTimeout(function(){ $("#container1").twentytwenty();}, 1000);
+// });
